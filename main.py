922713c836e26745997202e1a01fdc54d1a3e2b9
tsudo <tosei.sudo@gmail.com>
2025-07-13 20:26:59 +0900

    add score model

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 import copy
 
 from data import get_random_data
+from model import SetModel
 
 start = time.time()
 
@@ -19,6 +20,8 @@ print(data.shape)
 
 # ここまでデータ生成
 
+model_dict = {model.id: model for model in model_data}
+
 n = 3
 
 max_y = data[:, 1].max()
@@ -28,10 +31,10 @@ x_diff = 360 / n
 y_diff = (max_y - min_y) / n
 
 # Extend the data to create a grid-like structure
-extend_model_data = copy.deepcopy(model_data)
-indexies = np.where(data[:, 0] < x_diff * (n - 1))
+extend_id_data = [model.id for model in model_data]
+indexies = np.where(data[:, 0] < (x_diff * (n - 1)))
 for index in indexies[0]:
-    extend_model_data.append(model_data[index])
+    extend_id_data.append(model_data[index].id)
 
 extend_data = np.append(data, data[(data[:, 0] < (x_diff * (n - 1))), :] + np.array([[360, 0]]), axis=0)
 
@@ -46,8 +49,9 @@ pairing_index = []
 
 for i in range(n):
     ideal_coordinates = data + np.array([[x_diff * i, y_diff * i]])
-    ideal_coordinates[(ideal_coordinates[:, 1] > max_y), 1] = min_y + (ideal_coordinates[(ideal_coordinates[:, 1] > max_y), 1] - max_y)
-    
+    mask = (ideal_coordinates[:, 1] > max_y)
+    ideal_coordinates[mask, 1] = min_y + (ideal_coordinates[mask, 1] - max_y)
+
     normalized_ideal_coordinates = np.array([
         ideal_coordinates[:, 0] / 360,
         (ideal_coordinates[:, 1] - min_y) / (max_y - min_y)
@@ -59,19 +63,18 @@ for i in range(n):
 
 pairing_index = np.array(pairing_index).transpose(1, 0)
 
-for row in pairing_index:
-    pairing_model = [extend_model_data[i] for i in row]
-    
-    print([m.id for m in pairing_model])
+score_models = [SetModel(0, [model_dict.get(extend_id_data[i]) for i in row]) for row in pairing_index]
 
-# print(time.time() - start)
+print(score_models)
 
-# plt.scatter(extend_data[:, 0], extend_data[:, 1], alpha=0.5)
-# for pairing in pairing_index:
-#     plt.plot(extend_data[pairing, 0], extend_data[pairing, 1], alpha=0.5)
+print(time.time() - start)
 
-# plt.title('Random Scatter Plot')
-# plt.xlabel('X-axis')
-# plt.ylabel('Y-axis')
+plt.scatter(extend_data[:, 0], extend_data[:, 1], alpha=0.5)
+for pairing in pairing_index:
+    plt.plot(extend_data[pairing, 0], extend_data[pairing, 1], alpha=0.5)
 
-# plt.show()
+plt.title('Random Scatter Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+plt.show()
--- model.py
+++ model.py
@@ -2,6 +2,15 @@
 
 import uuid
 
+class SetModel:
+    def __init__(self, score=0, models=[]):
+        self.score = score
+        self.models = models
+    
+    def calc_score(self, time_weight=1.0, quality_weight=1.0, sensor_label_weight=1.0, meta_weight=1.0):
+        if not self.models:
+            return 0
+
 class Model:
     def __init__(self, unix_time, quality, sensor_label, meta_1, meta_2):
         self.id = uuid.uuid4().hex
